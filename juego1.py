b78cb6f1a9ddf38488dbb7052bb1aa60e5883d0e
vicevalck <vicevalck@estudiante.uc.cl>
2026-08-18 13:21:03 -0400

    funcion spock y tijera listas

--- juego1.py
+++ juego1.py
@@ -1,4 +1,28 @@
+#funcion spock
+def spock(item_enemigo):
+    if item_enemigo == 'tijera':
+        print('Spock destruye tijera')
+        print('Ganaste!')
+    elif item_enemigo == 'papel':
+        print('Papel desaprueba a Spock')
+        print('Perdiste')
+    elif item_enemigo == 'piedra':
+        print('Spock vaporiza piedra')
+        print('Ganaste!')
+    elif item_enemigo == 'lagarto':
+        print('Lagarto envenena a Spock')
+        print('Perdiste')
 
-print('hello world')
-print("holi")
-
+def tijera(item_enemigo):
+    if item_enemigo == 'spock':
+        print('Spock destruye tijera')
+        print('Perdiste')
+    elif item_enemigo == 'papel':
+        print('Tijera corta papel')
+        print('Ganaste!')
+    elif item_enemigo == 'piedra':
+        print('Piedra rompe tijera')
+        print('Perdiste')
+    elif item_enemigo == 'lagarto':
+        print('Tijera decapita lagarto')
+        print('Ganaste!')
