7807c9a1fdf34e955f846e8c8a6112733e69fc87
vicevalck <vicevalck@estudiante.uc.cl>
2026-08-18 20:31:28 -0400

    aaa

--- juego1.py
+++ juego1.py
@@ -82,36 +82,3 @@ def papel(item_enemigo):
     elif item_enemigo == "lagarto":
         print("Lagarto come papel")
         print("Perdiste")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def papel(item_enemigo):
-    if item_enemigo == "piedra":
-        print("Papel cubre piedra")
-        print("Ganaste!")
-
-    elif item_enemigo == "tijera":
-        print("Tijera corta papel")
-        print("Perdiste")
-
-    elif item_enemigo == "spock":
-        print('Papel desaprueba a Spock')
-        print("Ganaste!")
-
-    elif item_enemigo == "lagarto":
-        print("Lagarto come papel")
-        print("Perdiste")
