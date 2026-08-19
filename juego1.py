a2d069a4e1e2caa830e97808d6d7696af26d5908
flori <florencia.ruiz@estudiante.uc.cl>
2026-08-18 20:11:28 -0400

    lista funcion piedra

--- juego1.py
+++ juego1.py
@@ -47,3 +47,21 @@ def lagarto(item_enemigo):
     elif item_enemigo == "spock":
         print("Lagarto envenena a spock")
         print("Ganaste!")
+
+
+def piedra(item_enemigo):
+    if item_enemigo == "tijera":
+        print("Piedra rompe tijera")
+        print("Ganaste!")
+
+    elif item_enemigo == "papel":
+        print("Papel cubre piedra")
+        print("Perdiste")
+
+    elif item_enemigo == "spock":
+        print("Spock vaporiza piedra")
+        print("Perdiste")
+
+    elif item_enemigo == "lagarto":
+        print("Piedra aplasta lagarto")
+        print("Ganaste!")
